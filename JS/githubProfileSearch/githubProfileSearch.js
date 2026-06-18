1908442a2295f42992779c3b0c37fef46e9cb7c9
saqib shah <sa2870648@gmail.com>
2026-06-18 10:57:36 +0500

    add feature(fetch user repos)

--- JS/githubProfileSearch/githubProfileSearch.js
+++ JS/githubProfileSearch/githubProfileSearch.js
@@ -10,22 +10,38 @@ const totFollowers = document.getElementById("total-followers");
 const totFollowing = document.getElementById("total-following");
 const totRepos = document.getElementById("total-repos");
 const profileLink = document.getElementById("profileLink");
+const loadingMsg = document.getElementById("loadingMsg");
 
 
 searchBtn.addEventListener("click",async()=>{
-    const userName = searchInput.value.trim();    
+    const userName = searchInput.value.trim(); 
+
     if(userName === "") return;
     errorMsg.textContent = "";
-    
+    document.getElementById("profile-card").classList.add("hidden"); 
+    document.getElementById("repos-container").classList.add("hidden");
+    loadingMsg.classList.remove("hidden"); // SHOW loading
+    searchBtn.style.pointerEvents = "none";
+
+        
     try {
         console.log("api is calling.............");
         const userData = await searchUser(userName);
         console.log("show user data");
         
         displayUserData(userData);
+
+        const repos = await fetchUserRepos(userName); 
+        displayRepos(repos);
         
     } catch (error) {
         errorMsg.textContent = error.message;
+        document.getElementById("profile-card").classList.add("hidden"); 
+        document.getElementById("repos-container").classList.add("hidden");
+    }
+    finally{
+        loadingMsg.classList.add("hidden");
+        searchBtn.style.pointerEvents = "auto";
     }
 });
 
@@ -35,19 +51,45 @@ searchInput.addEventListener("keydown",(event)=>{
     }    
 });
 
-async function searchUser(userName) {
-    try {        
+async function searchUser(userName) {       
         const response = await fetch(`https://api.github.com/users/${userName}`);
         if(!response.ok){
             throw new Error("User not found");
         }
         const userData = await response.json();
-        console.log("api is called...........");
-        
+        console.log("api is called...........");        
         return userData;
-    } catch (error) {
-        console.log("error",error.message);        
+}
+
+async function fetchUserRepos(user_name) {
+     const response = await fetch(`https://api.github.com/users/${user_name}/repos`);
+     
+    if (!response.ok) {
+        throw new Error("Could not load repositories");
     }
+    const repos = await response.json();
+    console.log(" repos json data ",response);
+    
+    return repos;
+
+}
+
+function displayRepos(userRepos){
+    const repoList = document.getElementById("repos-list");
+    repoList.innerHTML = "";
+
+    if(userRepos.length === 0){
+        repoList.innerHTML = "<li>No public Repository found.</li>";
+    }
+    
+    userRepos.forEach((repo) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            <span>⭐ ${repo.stargazers_count}</span>
+        `; 
+        repoList.appendChild(li);
+    });
+    document.getElementById("repos-container").classList.remove("hidden");
 }
 
 function displayUserData(userdata){
