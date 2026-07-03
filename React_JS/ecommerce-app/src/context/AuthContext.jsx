5c510b070b150d08efb8ffc7dc75f57b6b1247c0
saqib shah <sa2870648@gmail.com>
2026-07-03 20:04:30 +0500

    add layout page and navbar component

--- React_JS/ecommerce-app/src/context/AuthContext.jsx
+++ React_JS/ecommerce-app/src/context/AuthContext.jsx
@@ -1,1 +1,36 @@
 // (mock auth)
+import {  createContext ,useState,useEffect,useContext} from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({children}) =>{
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const savedUser = localStorage.getItem("user");
+        if(savedUser){
+            setUser(JSON.parse(savedUser));
+        }
+        setLoading(false);
+    }, [])
+
+    const login = (userData) =>{
+        setUser(userData);
+        localStorage.setItem("user", JSON.stringify(userData));
+    }
+
+    const logout = () =>{
+        setUser(null);
+        localStorage.removeItem("user");
+    }
+
+    return(
+        <AuthContext.Provider value= {{user, loading, login, logout}}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = useContext(AuthContext);
--- React_JS/ecommerce-app/src/pages/Login.jsx
+++ React_JS/ecommerce-app/src/pages/Login.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import {useAuth} from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const {login} = useAuth();
+  const navigate = useNavigate();
   return (
     <div>
       Login Page
